5d056d506a61080ee7bc89318239a4e73e7b4734
David Navarro Estruch <david@elasticbox.com>
2015-09-26 22:21:49 +0200

    Implement makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Use makeCacheMatrix and cacheSolve to reuse the computed inverse value of a matrix
+## The matrix must be invertible
+## Example:
+## m <- rbind(c(1, -1/2), c(-1/2, 1))
+## mCached <- makeCacheMatrix(m)
+## mSolved <- cacheSolve(m)
+## mCached$get()  # Get the original value `m`
 
-## Write a short comment describing this function
+
+## makeCacheMatrix returns a variable that can be used with cacheSolve
+## The variable supports the following uses:
+## m <- makeCacheMatrix(x)
+## m$get() # Get the original matrix
+## m$set(x2) # Save new matrix x2
+## m$getinverse() # Get the cached value of the inverse. Use `cacheSolve` instead
+## m$setinverse() # Set the cached value of the inverse. Use `cacheSolve` instead
 
 makeCacheMatrix <- function(x = matrix()) {
+    cached <- NULL
 
+    # Getter and setter for x
+    set <- function(y) {
+        x <<- y
+        cached <<- NULL
+    }
+    get <- function() x
+
+    # Getter and setter for the cached value
+    setinverse <- function(inverse) cached <<- inverse
+    getinverse <- function() cached
+
+    invisible(list(
+        set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse))
 }
 
 
-## Write a short comment describing this function
+## cacheSolve gets a variable produced by makeCacheMatrix and returns the inverse of the matrix, computing it if needed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    cached <- x$getinverse()
+    if(!is.null(cached)) {
+        message("getting cached data")
+        return(cached)
+    }
+    data <- x$get()
+    cached <- solve(data, ...)
+    x$setinverse(cached)
+    cached
 }
